dd7402af22fd9f8e145f2f4f20bcfbe8f76ae314
mmartins-vitor <mmartins.vitor@hotmail.com>
2023-10-05 11:40:24 -0300

    correção nº2

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIASRIZ57LHNYLXUSYQ"
-  secret_key = "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"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_access_key" {
+  type        = string
+  description = "Access key"
+}
+variable "aws_secret_key" {
+  type        = string
+  description = "Secret key"
+}
